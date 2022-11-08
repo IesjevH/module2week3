f0a17cae4bc92698d6c88a2629994dd61bc917fd
Isis <isisvanhemert@gmail.com>
2022-11-08 14:06:36 +0100

    init

--- eindpdracht/bestelpagina/main.js
+++ eindpdracht/bestelpagina/main.js
@@ -1,5 +1,5 @@
 const verzendButton = document.getElementById("verzend");
-const woonplaats = document.getElementById("woonplaats")
+const woonplaats = document.getElementById("woonplaats");
 verzendButton.addEventListener("click", storeItems);
 
 console.log(verzendButton)
