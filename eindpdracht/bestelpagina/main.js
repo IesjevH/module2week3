a887143254004e49781212ae5d28f6dab210731e
Isis <isisvanhemert@gmail.com>
2022-11-09 10:37:32 +0100

    init

--- eindpdracht/bestelpagina/main.js
+++ eindpdracht/bestelpagina/main.js
@@ -1,9 +1,13 @@
 const verzendButton = document.getElementById("verzend");
 const woonplaats = document.getElementById("woonplaats");
+const voornaam = document.getElementById("voornaam");
+const Tussenv = document.getElementById("Tussenv");
 verzendButton.addEventListener("click", storeItems);
 
 console.log(verzendButton)
 
 function storeItems(){
     localStorage.setItem("woonplaats",woonplaats.value)
+    localStorage.setItem("voornaam",voornaam.value)
+    localStorage.setItem("Tussenv",Tussenv.value)
 }
